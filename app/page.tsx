419d69e3e5dd05a323930080da85891baf95b99a
Stefan <floydrise@gmail.com>
2025-03-25 18:24:24 +0200

    Remove unused variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import { KaplayContext } from "@/app/Kaplay/KaplayContext";
 import { SignOut } from "@/components/SignOutBtn";
 import { auth } from "@/auth";
-import { fetchPlayerScores } from "@/lib/actions";
 
 export default async function Home() {
   const session = await auth();
-  const result = await fetchPlayerScores(Number(session?.user?.id));
-  const playerScores: number[] = result.flatMap((value) =>
-    Object.values(value),
-  );
   return (
     <div className={"flex  w-full h-full"}>
       <KaplayContext
